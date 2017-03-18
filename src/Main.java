256a6d92354dd9a70aa737e51a15df458c40ec63
kowalewskitomasz <tomasz.kowalewski6@gmail.com>
2017-03-19 00:09:08 +0100

    Dodanie ruchu w,s,a,d dla drugiej kuli

--- src/Main.java
+++ src/Main.java
@@ -16,16 +16,27 @@ import javafx.util.Duration;
 
 public class Main extends Application {
     private static final int      KEYBOARD_MOVEMENT_DELTA = 1;
-    private static final Duration TRANSLATE_DURATION      = Duration.seconds(0.25);
+    //private static final Duration TRANSLATE_DURATION      = Duration.seconds(0.25);
     final BooleanProperty upPressed = new SimpleBooleanProperty(false);
     final BooleanProperty downPressed = new SimpleBooleanProperty(false);
     final BooleanProperty rightPressed = new SimpleBooleanProperty(false);
     final BooleanProperty leftPressed = new SimpleBooleanProperty(false);
 
+    final BooleanProperty wPressed = new SimpleBooleanProperty(false);
+    final BooleanProperty sPressed = new SimpleBooleanProperty(false);
+    final BooleanProperty aPressed = new SimpleBooleanProperty(false);
+    final BooleanProperty dPressed = new SimpleBooleanProperty(false);
+
     final BooleanBinding upAndRightPressed = upPressed.and(rightPressed);
     final BooleanBinding downAndRightPressed = downPressed.and(rightPressed);
     final BooleanBinding upAndLeftPressed = upPressed.and(leftPressed);
     final BooleanBinding downAndLeftPressed = downPressed.and(leftPressed);
+
+    final BooleanBinding wAnddPressed = wPressed.and(dPressed);
+    final BooleanBinding sAnddPressed = sPressed.and(dPressed);
+    final BooleanBinding wAndaPressed = wPressed.and(aPressed);
+    final BooleanBinding sAndaPressed = sPressed.and(aPressed);
+
 
     Runnable task;
     Thread thread;
@@ -33,20 +44,21 @@ public class Main extends Application {
     public static void main(String[] args) { launch(args); }
     @Override public void start(Stage stage) throws Exception {
         final Circle circle = createCircle();
-        final Group group = new Group(createInstructions(), circle);
-        final TranslateTransition transition = createTranslateTransition(circle);
+        final Circle circle2 = createCircle2();
+        final Group group = new Group(createInstructions(), circle, circle2);
+        //final TranslateTransition transition = createTranslateTransition(circle);
 
-        final Scene scene = new Scene(group, 600, 400, Color.CORNSILK);
+        final Scene scene = new Scene(group, 800, 600, Color.CORNSILK);
         moveCircleOnKeyPress(scene, circle);
-        moveCircleOnMousePress(scene, circle, transition);
+        moveCircleOnKeyPress2(scene, circle2);
+        //moveCircleOnMousePress(scene, circle, transition);
 
         task = () ->
-            dlugoTrwaleObliczenia(circle);
+            dlugoTrwaleObliczenia(circle, circle2);
 
 
         thread = new Thread(task);
         thread.start();
-
         stage.setScene(scene);
         stage.show();
         stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
@@ -67,25 +79,31 @@ public class Main extends Application {
     }
 
     private Circle createCircle() {
-        final Circle circle = new Circle(200, 150, 50, Color.BLUEVIOLET);
+        final Circle circle = new Circle(800, 600, 50, Color.BLUEVIOLET);
         circle.setOpacity(0.7);
         return circle;
     }
 
-    private TranslateTransition createTranslateTransition(final Circle circle) {
-        final TranslateTransition transition = new TranslateTransition(TRANSLATE_DURATION, circle);
-        transition.setOnFinished(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent t) {
-                circle.setCenterX(circle.getTranslateX() + circle.getCenterX());
-                circle.setCenterY(circle.getTranslateY() + circle.getCenterY());
-                circle.setTranslateX(0);
-                circle.setTranslateY(0);
-            }
-        });
-        return transition;
+    private Circle createCircle2() {
+        final Circle circle = new Circle(400, 300, 50, Color.GREENYELLOW);
+        circle.setOpacity(0.7);
+        return circle;
     }
 
-    void dlugoTrwaleObliczenia(Circle circle) {
+//    private TranslateTransition createTranslateTransition(final Circle circle) {
+//        final TranslateTransition transition = new TranslateTransition(TRANSLATE_DURATION, circle);
+//        transition.setOnFinished(new EventHandler<ActionEvent>() {
+//            @Override public void handle(ActionEvent t) {
+//                circle.setCenterX(circle.getTranslateX() + circle.getCenterX());
+//                circle.setCenterY(circle.getTranslateY() + circle.getCenterY());
+//                circle.setTranslateX(0);
+//                circle.setTranslateY(0);
+//            }
+//        });
+//        return transition;
+//    }
+
+    void dlugoTrwaleObliczenia(Circle circle, Circle circle2) {
         while (!Thread.interrupted()) {
             if (upAndLeftPressed.get()) {
                 circle.setCenterX(circle.getCenterX() - KEYBOARD_MOVEMENT_DELTA);
@@ -116,6 +134,37 @@ public class Main extends Application {
                 circle.setCenterY(circle.getCenterY() + KEYBOARD_MOVEMENT_DELTA);
             }
 
+
+            if (wAndaPressed.get()) {
+                circle2.setCenterX(circle2.getCenterX() - KEYBOARD_MOVEMENT_DELTA);
+                circle2.setCenterY(circle2.getCenterY() - KEYBOARD_MOVEMENT_DELTA);
+            }
+            else if (wAnddPressed.get()) {
+                circle2.setCenterX(circle2.getCenterX() + KEYBOARD_MOVEMENT_DELTA);
+                circle2.setCenterY(circle2.getCenterY() - KEYBOARD_MOVEMENT_DELTA);
+            }
+            else if (sAndaPressed.get()) {
+                circle2.setCenterX(circle2.getCenterX() - KEYBOARD_MOVEMENT_DELTA);
+                circle2.setCenterY(circle2.getCenterY() + KEYBOARD_MOVEMENT_DELTA);
+            }
+            else if (sAnddPressed.get()) {
+                circle2.setCenterX(circle2.getCenterX() + KEYBOARD_MOVEMENT_DELTA);
+                circle2.setCenterY(circle2.getCenterY() + KEYBOARD_MOVEMENT_DELTA);
+            }
+            else if (wPressed.get()) {
+                circle2.setCenterY(circle2.getCenterY() - KEYBOARD_MOVEMENT_DELTA);
+            }
+            else if (dPressed.get()) {
+                circle2.setCenterX(circle2.getCenterX() + KEYBOARD_MOVEMENT_DELTA);
+            }
+            else if (aPressed.get()) {
+                circle2.setCenterX(circle2.getCenterX() - KEYBOARD_MOVEMENT_DELTA);
+            }
+            else if (sPressed.get()) {
+                circle2.setCenterY(circle2.getCenterY() + KEYBOARD_MOVEMENT_DELTA);
+            }
+
+
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e){
@@ -126,41 +175,105 @@ public class Main extends Application {
 
     private void moveCircleOnKeyPress(Scene scene, final Circle circle) {
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override public void handle(KeyEvent event) {
+            @Override
+            public void handle(KeyEvent event) {
                 switch (event.getCode()) {
-                    case UP:    upPressed.set(true); break;
-                    case RIGHT: rightPressed.set(true); break;
-                    case DOWN:  downPressed.set(true); break;
-                    case LEFT:  leftPressed.set(true); break;
+                    case UP:
+                        upPressed.set(true);
+                        break;
+                    case RIGHT:
+                        rightPressed.set(true);
+                        break;
+                    case DOWN:
+                        downPressed.set(true);
+                        break;
+                    case LEFT:
+                        leftPressed.set(true);
+                        break;
                 }
 
             }
+
         });
+
 
         scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
-            @Override public void handle(KeyEvent event) {
+            @Override
+            public void handle(KeyEvent event) {
                 switch (event.getCode()) {
-                    case UP:    upPressed.set(false); break;
-                    case RIGHT: rightPressed.set(false); break;
-                    case DOWN:  downPressed.set(false); break;
-                    case LEFT:  leftPressed.set(false); break;
+                    case UP:
+                        upPressed.set(false);
+                        break;
+                    case RIGHT:
+                        rightPressed.set(false);
+                        break;
+                    case DOWN:
+                        downPressed.set(false);
+                        break;
+                    case LEFT:
+                        leftPressed.set(false);
+                        break;
                 }
             }
         });
+
+
     }
 
-    private void moveCircleOnMousePress(Scene scene, final Circle circle, final TranslateTransition transition) {
-        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent event) {
-                if (!event.isControlDown()) {
-                    circle.setCenterX(event.getSceneX());
-                    circle.setCenterY(event.getSceneY());
-                } else {
-                    transition.setToX(event.getSceneX() - circle.getCenterX());
-                    transition.setToY(event.getSceneY() - circle.getCenterY());
-                    transition.playFromStart();
+    private void moveCircleOnKeyPress2(Scene scene, final Circle circle) {
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case W:
+                        wPressed.set(true);
+                        break;
+                    case D:
+                        dPressed.set(true);
+                        break;
+                    case S:
+                        sPressed.set(true);
+                        break;
+                    case A:
+                        aPressed.set(true);
+                        break;
+                }
+
+            }
+
+        });
+        scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case W:
+                        wPressed.set(false);
+                        break;
+                    case D:
+                        dPressed.set(false);
+                        break;
+                    case S:
+                        sPressed.set(false);
+                        break;
+                    case A:
+                        aPressed.set(false);
+                        break;
                 }
             }
         });
-    }
-}
+    }}
+
+//    private void moveCircleOnMousePress(Scene scene, final Circle circle, final TranslateTransition transition) {
+//        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+//            @Override public void handle(MouseEvent event) {
+//                if (!event.isControlDown()) {
+//                    circle.setCenterX(event.getSceneX());
+//                    circle.setCenterY(event.getSceneY());
+//                } else {
+//                    transition.setToX(event.getSceneX() - circle.getCenterX());
+//                    transition.setToY(event.getSceneY() - circle.getCenterY());
+//                    transition.playFromStart();
+//                }
+//            }
+//        });
+//    }
