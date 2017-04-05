7686c3890411062fd3de8c48eec6b7b0708e6b55
Kali <stancelrobert@gmail.com>
2017-04-05 16:20:20 +0200

    Refactor into serializable data model.

--- src/business/game/Point.java
+++ src/business/game/Point.java
@@ -3,32 +3,36 @@ package business.game;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 
-public class Point {
-    private DoubleProperty x = new SimpleDoubleProperty();
-    private DoubleProperty y = new SimpleDoubleProperty();
+import java.io.Serializable;
+
+public class Point implements Serializable {
+    transient private DoubleProperty xProperty = new SimpleDoubleProperty();
+    transient private DoubleProperty yProperty = new SimpleDoubleProperty();
+    double x = 0.0;
+    double y = 0.0;
 
     public Point() {
-        this.x.set(0);
-        this.y.set(0);
+        this(0.0, 0.0);
     }
 
     public Point(double x, double y) {
-        this.x.set(x);
-        this.y.set(y);
+        this.setLocation(x, y);
     }
 
     public void setLocation(double x, double y) {
-        this.x.set(x);
-        this.y.set(y);
+        this.xProperty.set(x);
+        this.yProperty.set(y);
+        this.x = x;
+        this.y = y;
     }
 
     public double getX() {
-        return x.get();
+        return x;
     }
 
-    public double getY() { return y.get(); }
+    public double getY() { return y; }
 
-    public DoubleProperty getXProperty() { return x; }
+    public DoubleProperty getXProperty() { return xProperty; }
 
-    public DoubleProperty getYProperty() { return y; }
+    public DoubleProperty getYProperty() { return yProperty; }
 }
